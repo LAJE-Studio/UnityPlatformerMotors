02b5c57645859595f697b11a76cdfae11731ae74
DDevilISL <brunorbsf@gmail.com>
2018-04-23 22:07:57 -0300

    Adicionado projeto UPM-Editor

---
+++ UPM-Editor/Editor/StateMotorEditor.cs
@@ -0,0 +1,8 @@
+﻿using UnityEditor;
+
+namespace UPM.Editor {
+    [CustomEditor(typeof(StateMotor))]
+    public class StateMotorEditor : UnityEditor.Editor {
+        public override void OnInspectorGUI() { }
+    }
+}
