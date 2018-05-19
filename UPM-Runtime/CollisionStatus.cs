be505faf51631005544355cd1082c536d2cd698b
DDevilISL <brunorbsf@gmail.com>
2018-05-19 08:57:45 -0300

    Adicionado alguns métodos de utilidades em CollisionStatus

--- UPM-Runtime/CollisionStatus.cs
+++ UPM-Runtime/CollisionStatus.cs
@@ -3,6 +3,14 @@
 namespace UPM {
     [Serializable]
     public struct CollisionStatus {
+        [Flags]
+        public enum CollisionDirection {
+            Up = 1 << 0,
+            Down = 1 << 1,
+            Left = 1 << 2,
+            Right = 1 << 3
+        }
+
         public bool Up;
         public bool Down;
         public bool Left;
@@ -10,20 +18,74 @@ namespace UPM {
 
         public int HorizontalCollisionDir {
             get {
-                if (Left == Right) {
-                    return 0;
-                }
+                return GetCollisionDir(Right, Left);
+            }
+        }
 
-                if (Left) {
-                    return -1;
-                }
+        public int VerticalCollisionDir {
+            get {
+                return GetCollisionDir(Up, Down);
+            }
+        }
 
-                if (Right) {
-                    return 1;
-                }
-
+        private static int GetCollisionDir(bool positive, bool negative) {
+            if (positive == negative) {
                 return 0;
             }
+
+            if (positive) {
+                return -1;
+            }
+
+            return 1;
+        }
+
+        public bool HasAll() {
+            return Up && Down && Left && Right;
+        }
+
+        public bool HasAny() {
+            return Up || Down || Left || Right;
+        }
+
+        public bool HasAll(CollisionDirection direction) {
+            if ((direction & CollisionDirection.Up) == CollisionDirection.Up && !Up) {
+                return false;
+            }
+
+            if ((direction & CollisionDirection.Down) == CollisionDirection.Down && !Down) {
+                return false;
+            }
+
+            if ((direction & CollisionDirection.Left) == CollisionDirection.Left && !Left) {
+                return false;
+            }
+
+            if ((direction & CollisionDirection.Right) == CollisionDirection.Right && !Right) {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool HasAny(CollisionDirection direction) {
+            if ((direction & CollisionDirection.Up) == CollisionDirection.Up && Up) {
+                return true;
+            }
+
+            if ((direction & CollisionDirection.Down) == CollisionDirection.Down && Down) {
+                return true;
+            }
+
+            if ((direction & CollisionDirection.Left) == CollisionDirection.Left && Left) {
+                return true;
+            }
+
+            if ((direction & CollisionDirection.Right) == CollisionDirection.Right && Right) {
+                return true;
+            }
+
+            return false;
         }
     }
 }
